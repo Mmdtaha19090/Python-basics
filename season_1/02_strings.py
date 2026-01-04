8a31f21dac9760c3ccbaede1a2257bc722796c4f
Taha Farimani <tahafarimani.test@gmail.com>
2026-01-05 02:30:43 +0330

    Create 02_strings.py

---
+++ season_1/02_strings.py
@@ -0,0 +1,17 @@
+# season 1 - strings
+text = "hello python"
+print(text)
+
+# length of string
+print(len(text))
+
+# indexing
+print(text[0])
+print(text[-1])
+
+# concatenation
+name = "taha"
+print("hello " + name)
+
+# repetition
+print(name*3)
