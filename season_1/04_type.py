15fe3fbdbc1984b73abf0a257c66335f7b099886
Taha Farimani <tahafarimani.test@gmail.com>
2026-01-05 02:49:12 +0330

    Create 04_type.py

---
+++ season_1/04_type.py
@@ -0,0 +1,21 @@
+# season 1 - Type and Input
+
+# get input from user
+age = input("Enter your age : ")
+print(age)
+print(type(age))
+
+# convert input to integer
+age = int(age)
+print(age)
+print(type(age))
+
+# float input
+height = float(input("Enter your height in meters : "))
+print(height)
+print(type(height))
+
+# boolean example
+is_student = True
+print(is_student)
+print(type(is_student))
