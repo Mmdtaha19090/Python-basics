64744ef76d80f7ac40d83306b0692eea6010975f
Taha Farimani <102111158+Mmdtaha19090@users.noreply.github.com>
2026-01-04 22:48:14 +0330

    Create 01_variables.py

---
+++ season_1/01_variables.py
@@ -0,0 +1,16 @@
+#Season 1 - Variable and print
+#Exercise 1 : integer variable 
+age = 18
+print(age)
+
+#Exercise 2: string variable
+name = "Taha"
+print(name)
+
+#Exercise 3 :boolean variable
+is_student = True
+print(is_student)
+
+#Exercise 4 : change variable value
+age = 19
+print(age)
