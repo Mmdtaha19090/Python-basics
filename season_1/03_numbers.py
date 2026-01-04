e466cad265a616d18a0b37e4146ddeedf003a228
Taha Farimani <tahafarimani.test@gmail.com>
2026-01-05 02:39:35 +0330

    Create 03_numbers.py

---
+++ season_1/03_numbers.py
@@ -0,0 +1,25 @@
+# season 1 - Numbers and boolean
+
+# integers
+a = 10
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+# float
+x = 2.5
+y = 1.2
+print(x+y)
+
+# boolean values
+is_python_easy = True
+is_java_hard = False
+print(is_python_easy)
+print(is_java_hard)
+
+# comparisons
+print(a > b)
+print(a == b)
+print(a != b)
