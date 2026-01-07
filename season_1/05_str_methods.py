8be93ca2568b8bee871d19aa152f9e81888b13fc
Taha Farimani <tahafarimani.test@gmail.com>
2026-01-07 17:05:35 +0330

    Create 05_str_methods.py

---
+++ season_1/05_str_methods.py
@@ -0,0 +1,17 @@
+# String Methods - Season 1
+
+text = "   Hello World Python   "
+
+# lower & upper
+print(text.lower())
+print(text.upper())
+
+# strip (remove spaces from start and end)
+print(text.strip())
+
+# replace
+print(text.replace("Python", "Programming"))
+
+# split
+words = text.split()
+print(words)
