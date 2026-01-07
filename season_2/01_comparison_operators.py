8ae199e374c2d6039c452e1258cafd1bb1349c1f
Taha Farimani <tahafarimani.test@gmail.com>
2026-01-07 18:08:48 +0330

    Create 01_comparison_operators.py

---
+++ season_2/01_comparison_operators.py
@@ -0,0 +1,15 @@
+# Comparison and Logical Operators
+
+a = 10
+b = 5
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+print(a > 5 and b < 10)
+print(a > 5 or b > 10)
+print(not a == b)
