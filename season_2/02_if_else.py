82322d1a6d65b8363ff9004224da1744e7421ab2
Taha Farimani <tahafarimani.test@gmail.com>
2026-01-07 18:08:52 +0330

    Create 02_if_else.py

---
+++ season_2/02_if_else.py
@@ -0,0 +1,17 @@
+# if / elif / else
+
+age = 18
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Underage")
+
+score = 75
+
+if score >= 90:
+    print("Excellent")
+elif score >= 60:
+    print("Passed")
+else:
+    print("Failed")
