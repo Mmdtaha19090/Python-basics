0f4b4c7a75989666f34e2da9212a70739956a03c
Taha Farimani <tahafarimani.test@gmail.com>
2026-02-04 01:12:32 +0330

    Update 08_practice.py

--- practice_jadi_source/08_practice.py
+++ practice_jadi_source/08_practice.py
@@ -1,4 +1,10 @@
-# offer
+'''
+A store offers special discounts to its customers. The discount rules are as follows:
+If the purchase amount is more than 50,000 Tomans, a 20% discount is applied.
+If the purchase amount is between 20,000 and 50,000 Tomans, a 10% discount is applied.
+If the purchase amount is less than 20,000 Tomans, no discount is applied.
+Write a program that receives the purchase amount from the user and displays the final amount.
+'''
 buy = int(input())
 if 50000 < buy :
     buy = 0.8 * buy
