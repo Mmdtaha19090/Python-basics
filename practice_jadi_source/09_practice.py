fc8279ad21529a87b9adf4369c46cd0755323e34
Taha Farimani <tahafarimani.test@gmail.com>
2026-02-04 01:12:36 +0330

    Update 09_practice.py

--- practice_jadi_source/09_practice.py
+++ practice_jadi_source/09_practice.py
@@ -1,3 +1,10 @@
+''' A magician has decided to increase the power of numbers using a special spell!
+He chooses a number n as the number of steps and a number x on which the operation is to be performed.
+Then he does the following:
+If the number is odd, he doubles its value and subtracts 1 unit.
+If the number is even, he halves its value.
+The magician performs this operation exactly n times. '''
+
 n = int(input())
 x = int(input())
 for i in range(n) :
