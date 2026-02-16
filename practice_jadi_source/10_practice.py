55ba59f03ea41ad90b2a25ea89ef404271c85278
Taha Farimani <tahafarimani.test@gmail.com>
2026-02-16 20:07:42 +0330

    Update 10_practice.py

--- practice_jadi_source/10_practice.py
+++ practice_jadi_source/10_practice.py
@@ -1,1 +1,16 @@
-#khali
+'''
+We have a machine that takes n and if it is even , halve it , and if it is odd , multiply it by three and add one.
+Print the spteps to convert this number to one...
+'''
+n = int(input())
+while n != 1 :
+    if n %2 == 0 :
+        n = int(n / 2)
+        if n==1:
+            print(n)
+            break
+        print(n,end=" -> ")
+        
+    else :
+        n = int((3 * n) + 1)
+        print(n, end=" -> ")
